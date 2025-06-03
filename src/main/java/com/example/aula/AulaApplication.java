d58a515aaaa21c1440b7a1098110e2b095d2eebd
Pedro Lago <159453815+PedroLagoX@users.noreply.github.com>
2025-06-03 10:37:23 -0300

    Add files via upload

--- src/main/java/com/example/aula/AulaApplication.java
+++ src/main/java/com/example/aula/AulaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AulaApplication {
 
+	// ok
 	public static void main(String[] args) {
 		SpringApplication.run(AulaApplication.class, args);
 	}
